b95ff74e5724a26dc780422da962c6f100a7e348
David Tranaeus <david.tranaeus@kundo.se>
2022-09-23 15:56:54 +0200

    Add vim-plug for neovim

--- nvim/init.lua
+++ nvim/init.lua
@@ -6,3 +6,26 @@ vim.o.relativenumber = true
 -- remap -> options that makes mapings work recursively (on by default)
 -- :map and :noremap are recursive and non-recursive mapping commands
 vim.keymap.set("i", "jk", "<ESC>", { noremap = true, silent = true })
+
+
+-- Plugins
+-- https://dev.to/vonheikemen/neovim-using-vim-plug-in-lua-3oom
+local Plug = vim.fn['plug#']
+vim.call('plug#begin', '~/.config/nvim/plugged')
+
+-- Git
+Plug 'airblade/vim-gitgutter'
+-- Plug 'tpope/vim-fugitive'
+-- Plug 'lewis6991/gitsigns.nvim'
+
+-- fzf is a fuzzy finder (using find as default command?) without any pre-made
+-- vim key bindings
+-- ripgrep is a search tool using regex patterns
+-- fzf.vim is a vim plugin which integrates fzf in vim for searching for files
+-- (find?) and for content (ripgrep)
+Plug('junegunn/fzf', {['do'] = vim.fn['fzf#install']})
+Plug 'junegunn/fzf.vim'
+
+vim.call('plug#end')
+
+-- require('gitsigns').setup()
