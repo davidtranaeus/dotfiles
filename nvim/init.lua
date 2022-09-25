603f869f21aabd8d881398ed346f4cf458f6d265
davidtranaeus <dtranaeus@gmail.com>
2022-09-25 19:00:26 +0200

    Add basic LSP configs for Python

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,46 +1,69 @@
 vim.o.termguicolors = true
 vim.o.number = true
 vim.o.relativenumber = true
+vim.o.completeopt = "menu"
+vim.g.mapleader = " "
 
--- https://stackoverflow.com/questions/3776117/what-is-the-difference-between-the-remap-noremap-nnoremap-and-vnoremap-mapping
+-- Key mapings
 -- remap -> options that makes mapings work recursively (on by default)
 -- :map and :noremap are recursive and non-recursive mapping commands
-vim.keymap.set("i", "jk", "<ESC>", { noremap = true, silent = true })
+local opts = { noremap=true, silent=true }
+vim.keymap.set("i", "jk", "<ESC>", opts)
+vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, opts)
 
 
--- Plugins
--- https://dev.to/vonheikemen/neovim-using-vim-plug-in-lua-3oom
-local Plug = vim.fn['plug#']
-vim.call('plug#begin', '~/.config/nvim/plugged')
-
--- Git
-Plug 'airblade/vim-gitgutter'
--- Plug 'tpope/vim-fugitive'
--- Plug 'lewis6991/gitsigns.nvim'
-
+local Plug = vim.fn["plug#"]
+vim.call("plug#begin", "~/.config/nvim/plugged")
 -- fzf is a fuzzy finder (using find as default command?) without any pre-made
 -- vim key bindings
 -- ripgrep is a search tool using regex patterns
 -- fzf.vim is a vim plugin which integrates fzf in vim for searching for files
 -- (find?) and for content (ripgrep)
-Plug('junegunn/fzf', {['do'] = vim.fn['fzf#install']})
-Plug 'junegunn/fzf.vim'
+Plug("junegunn/fzf", {["do"] = vim.fn["fzf#install"]})
+Plug "junegunn/fzf.vim"
 
-Plug 'neovim/nvim-lspconfig'
-vim.call('plug#end')
+Plug "projekt0n/github-nvim-theme"
 
--- require('gitsigns').setup()
+Plug "neovim/nvim-lspconfig"
+Plug "williamboman/mason.nvim"
+Plug "jose-elias-alvarez/null-ls.nvim"
+Plug "nvim-lua/plenary.nvim" -- required by null-ls
+vim.call("plug#end")
 
--- LSP configs
--- https://github.com/neovim/nvim-lspconfig
+require("github-theme").setup({
+	theme_style = "light_default"
+})
+
+-- Attach buffer settings after the LS attaches
 local on_attach = function(client, bufnr)
-  -- Enable completion triggered by <c-x><c-o>
-  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+	local bufopts = { noremap=true, silent=true, buffer=bufnr }
+
+	vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
+	vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
+
+	-- vim.api.nvim_buf_set_option(bufnr, "formatexpr", "v:lua.vim.lsp.formatexpr()")
+	vim.keymap.set("n", "gq", vim.lsp.buf.formatting, bufopts)
+	vim.keymap.set("v", "gq", vim.lsp.buf.range_formatting, bufopts)
 end
 
--- how can pyright installed with a global npm version be used when there is
--- another different local npm version
--- how does pyright know which local Python version is used
-require'lspconfig'.pyright.setup{
+-- lsp config for various servers: https://github.com/neovim/nvim-lspconfig
+-- Add a pyrightconfig.json for venv settings
+-- { "venv" : "name of virtual env", "venvPath" : "/Users/name/.pyenv/versions" }
+require("lspconfig").pyright.setup{
     on_attach = on_attach,
 }
+
+vim.diagnostic.config({virtual_text = false})
+
+-- null-ls, run traditional command line tools through lsp
+local null_ls = require("null-ls")
+null_ls.setup({
+    sources = {
+        null_ls.builtins.formatting.autopep8, -- black does not support ranged formatting
+        null_ls.builtins.formatting.black,
+	null_ls.builtins.formatting.isort,
+    },
+})
+
+-- nvim package manager for lsp, dap, linters, formatters
+require("mason").setup()
