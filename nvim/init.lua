a707e8fe3fd975f6627f75400aa6731316225836
David Tranaeus <david.tranaeus@kundo.se>
2022-09-28 12:13:18 +0200

    Add Treesitter

--- nvim/init.lua
+++ nvim/init.lua
@@ -28,10 +28,19 @@ Plug "neovim/nvim-lspconfig"
 Plug "williamboman/mason.nvim"
 Plug "jose-elias-alvarez/null-ls.nvim"
 Plug "nvim-lua/plenary.nvim" -- required by null-ls
+
+-- does the post update hook really work?
+Plug('nvim-treesitter/nvim-treesitter', {["do"] = vim.fn[":TSUpdate"]})
+
+
+-- Plug 'mhinz/vim-signify'
+-- Plug 'lewis6991/gitsigns.nvim'
 vim.call("plug#end")
 
+-- require('gitsigns').setup()
+
 require("github-theme").setup({
-	theme_style = "light_default"
+	theme_style = "light"
 })
 
 -- Attach buffer settings after the LS attaches
@@ -42,6 +51,7 @@ local on_attach = function(client, bufnr)
 	vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
 
 	-- vim.api.nvim_buf_set_option(bufnr, "formatexpr", "v:lua.vim.lsp.formatexpr()")
+	-- what are good bindings for formatting?
 	vim.keymap.set("n", "gq", vim.lsp.buf.formatting, bufopts)
 	vim.keymap.set("v", "gq", vim.lsp.buf.range_formatting, bufopts)
 end
@@ -62,8 +72,16 @@ null_ls.setup({
         null_ls.builtins.formatting.autopep8, -- black does not support ranged formatting
         null_ls.builtins.formatting.black,
 	null_ls.builtins.formatting.isort,
+	null_ls.builtins.diagnostics.flake8,
     },
 })
 
 -- nvim package manager for lsp, dap, linters, formatters
 require("mason").setup()
+
+
+require('nvim-treesitter.configs').setup({
+	highlight = {
+		enable = true,
+	}
+})
