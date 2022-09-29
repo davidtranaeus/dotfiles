ea04624c8d81e19b8385392f3c75b0580b8bf684
davidtranaeus <dtranaeus@gmail.com>
2022-09-29 15:05:17 +0200

    Fix default indentations

--- nvim/ftplugin/javascript.lua
+++ nvim/ftplugin/javascript.lua
@@ -1,2 +1,1 @@
-vim.o.expandtab = true -- Insert spaces on tab key
-vim.o.shiftwidth = 2 -- How many spaces a tab key is
+vim.o.shiftwidth = 2
--- nvim/init.lua
+++ nvim/init.lua
@@ -3,6 +3,14 @@ vim.o.number = true
 vim.o.relativenumber = true
 vim.o.completeopt = "menu"
 vim.g.mapleader = " "
+vim.o.expandtab = true -- always insert spaces
+vim.o.shiftwidth = 4 -- the indent amount
+vim.o.autoindent = true -- same indent as last row
+vim.o.smartindent = true -- for dicts etc.
+
+vim.cmd [[
+    filetype indent off " dont load indent file for file types
+]]
 
 -- Key mapings
 -- remap -> options that makes mapings work recursively (on by default)
@@ -33,27 +41,34 @@ Plug "nvim-lua/plenary.nvim" -- required by null-ls
 Plug('nvim-treesitter/nvim-treesitter', {["do"] = vim.fn[":TSUpdate"]})
 
 
--- Plug 'mhinz/vim-signify'
--- Plug 'lewis6991/gitsigns.nvim'
+-- Plug "mhinz/vim-signify"
+Plug "lewis6991/gitsigns.nvim"
+
+Plug "tpope/vim-surround"
 vim.call("plug#end")
 
--- require('gitsigns').setup()
+require('gitsigns').setup({
+    on_attach = function(bufnr)
+        local bufopts = { noremap=true, silent=true, buffer=bufnr }
+        -- focus window with <C-w> w
+        vim.keymap.set("n", "<leader>hp", package.loaded.gitsigns.preview_hunk, bufopts)
+    end
+})
 
 require("github-theme").setup({
-	theme_style = "light"
+    theme_style = "light"
 })
 
 -- Attach buffer settings after the LS attaches
 local on_attach = function(client, bufnr)
-	local bufopts = { noremap=true, silent=true, buffer=bufnr }
+    local bufopts = { noremap=true, silent=true, buffer=bufnr }
 
-	vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
-	vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
-
-	-- vim.api.nvim_buf_set_option(bufnr, "formatexpr", "v:lua.vim.lsp.formatexpr()")
-	-- what are good bindings for formatting?
-	vim.keymap.set("n", "gq", vim.lsp.buf.formatting, bufopts)
-	vim.keymap.set("v", "gq", vim.lsp.buf.range_formatting, bufopts)
+    vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
+    vim.api.nvim_buf_set_option(bufnr, "formatexpr", "v:lua.vim.lsp.formatexpr()")
+    -- for formatters which does not support ranged formatting (black)
+    vim.keymap.set("n", "<leader>gq", vim.lsp.buf.formatting, bufopts)
+    -- vim.keymap.set("v", "gq", vim.lsp.buf.range_formatting, bufopts)
 end
 
 -- lsp config for various servers: https://github.com/neovim/nvim-lspconfig
@@ -71,17 +86,18 @@ null_ls.setup({
     sources = {
         null_ls.builtins.formatting.autopep8, -- black does not support ranged formatting
         null_ls.builtins.formatting.black,
-	null_ls.builtins.formatting.isort,
-	null_ls.builtins.diagnostics.flake8,
+        null_ls.builtins.formatting.isort,
+        null_ls.builtins.diagnostics.flake8,
     },
 })
 
 -- nvim package manager for lsp, dap, linters, formatters
+-- uses global nodejs version
 require("mason").setup()
 
-
+-- :TSInstall python
 require('nvim-treesitter.configs').setup({
-	highlight = {
-		enable = true,
-	}
+    highlight = {
+        enable = true,
+    }
 })
