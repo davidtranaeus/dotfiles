709f4b566283f05cdd8ae58156e521ac8d01f730
David Tranaeus <david.tranaeus@kundo.se>
2022-10-05 16:47:31 +0200

    Add nvim-cmp tab completion instead of omnifunc

--- nvim/lua/lsp/init.lua
+++ nvim/lua/lsp/init.lua
@@ -1,40 +1,50 @@
 local on_attach = function(client, bufnr)
     local bufopts = { noremap=true, silent=true, buffer=bufnr }
 
-    vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
     vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
     vim.keymap.set('i', '<C-k>', vim.lsp.buf.signature_help, bufopts)
     vim.api.nvim_buf_set_option(bufnr, "formatexpr", "v:lua.vim.lsp.formatexpr()")
     vim.keymap.set("n", "<leader>f", vim.lsp.buf.formatting, bufopts)
 end
 
---local cmp = require'cmp'
---cmp.setup({
---    window = {
---      documentation = cmp.config.disable
---    },
---    mapping = cmp.mapping.preset.insert({
---        -- set to true if enter should default to first option
---        ['<CR>'] = cmp.mapping.confirm({ select = false }),
---    }),
---    sources = cmp.config.sources(
---        {
---            { name = 'nvim_lsp', keyword_length = 3 },
---        }, 
---        {
---            { name = 'buffer', keyword_length = 3 },
---        }
---    ),
---    completion = {
---        autocomplete = false,
---    }
---})
---local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local has_words_before = function()
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match('%s') == nil
+end
+
+local cmp = require'cmp'
+cmp.setup({
+    window = {
+      documentation = cmp.config.disable
+    },
+    mapping = {
+        ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.confirm({ select = true }) -- default to first item
+            elseif vim.bo.buftype ~= 'prompt' and has_words_before() then
+                cmp.complete() -- open menu
+            else
+                fallback() -- regular tab
+            end
+        end),
+        ["<C-p>"] = cmp.mapping.select_prev_item(),
+        ["<C-n>"] = cmp.mapping.select_next_item(),
+    },
+    sources = {
+        { name = 'nvim_lsp' },
+        { name = 'buffer' },
+    },
+    completion = {
+        autocomplete = false,
+    },
+})
+local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 local servers = { "pyright", "tsserver" }
 for _, lsp in ipairs(servers) do
     require("lspconfig")[lsp].setup({
         on_attach = on_attach,
+        capabilities = capabilities,
     })
 end
 
