7d2b969dbe7848a5a005bbc96b5726cb9507f85f
David Tranaeus <david.tranaeus@kundo.se>
2022-10-10 16:40:24 +0200

    Add basic nvim-tree config

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -20,7 +20,28 @@ Plug "tpope/vim-surround"
 Plug 'hrsh7th/nvim-cmp'
 Plug 'hrsh7th/cmp-nvim-lsp'
 Plug 'hrsh7th/cmp-buffer'
+
+-- Plug 'nvim-tree/nvim-web-devicons'
+Plug 'nvim-tree/nvim-tree.lua'
 vim.call("plug#end")
+
+-- disable netrw at the very start of your init.lua (strongly advised)
+vim.g.loaded = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- empty setup using defaults
+require("nvim-tree").setup({
+    renderer = {
+        icons = {
+            show = {
+                file = false,
+                folder = false,
+                folder_arrow = false,
+                git = false,
+            }
+        }
+    }
+})
 
 require('gitsigns').setup({
     on_attach = function(bufnr)
