146cb98e73f094ddfe3ad81862bf157e5fa7f76d
davidtranaeus <dtranaeus@gmail.com>
2022-10-24 08:57:42 +0200

    Set scrolloff

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -7,6 +7,7 @@ vim.o.shiftwidth = 4 -- the indent amount
 vim.o.autoindent = true -- same indent as last row
 vim.o.smartindent = true -- for dicts etc.
 vim.o.signcolumn = "yes" -- prevents the buffer from jumping
+vim.o.scrolloff = 10
 
 vim.cmd [[
     filetype indent off " dont load indent file for file types
