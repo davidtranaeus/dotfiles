b59df9a1201aae57bcec125729978abaf06f5816
davidtranaeus <dtranaeus@gmail.com>
2022-10-30 16:46:30 +0100

    Add git staging mappings

--- nvim/lua/plugins/configs.lua
+++ nvim/lua/plugins/configs.lua
@@ -8,6 +8,8 @@ require('gitsigns').setup({
     local bufopts = { noremap=true, silent=true, buffer=bufnr }
     -- focus window with <C-w> w
     vim.keymap.set("n", "<leader>hp", gs.preview_hunk, bufopts)
+    vim.keymap.set({"n", "v"}, "<leader>hs", ":Gitsigns stage_hunk<CR>", bufopts)
+    vim.keymap.set({'n', 'v'}, '<leader>hr', ':Gitsigns reset_hunk<CR>', bufopts)
   end,
   signs = {
     add = { text = "+" },
