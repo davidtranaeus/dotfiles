eed0f6400f6b1af82a7f51ef161a4b112d52566d
David Tranaeus <david.tranaeus@kundo.se>
2022-10-31 16:16:20 +0100

    Change keymaps for fzf, rg, and buffer search

--- nvim/lua/plugins/configs.lua
+++ nvim/lua/plugins/configs.lua
@@ -2,14 +2,14 @@ require("github-theme").setup({ theme_style = "dark" })
 
 require("mason").setup()
 
-require('gitsigns').setup({
+require("gitsigns").setup({
   on_attach = function(bufnr)
     local gs = package.loaded.gitsigns
     local bufopts = { noremap=true, silent=true, buffer=bufnr }
-    -- focus window with <C-w> w
+
     vim.keymap.set("n", "<leader>hp", gs.preview_hunk, bufopts)
     vim.keymap.set({"n", "v"}, "<leader>hs", ":Gitsigns stage_hunk<CR>", bufopts)
-    vim.keymap.set({'n', 'v'}, '<leader>hr', ':Gitsigns reset_hunk<CR>', bufopts)
+    vim.keymap.set({"n", "v"}, "<leader>hr", ":Gitsigns reset_hunk<CR>", bufopts)
   end,
   signs = {
     add = { text = "+" },
@@ -20,9 +20,9 @@ require('gitsigns').setup({
 
 require("telescope").load_extension("fzf")
 local builtin = require("telescope.builtin")
-vim.keymap.set("n", "ff", builtin.find_files, {})
-vim.keymap.set("n", "fg", builtin.live_grep, {})
-vim.keymap.set("n", "fb", builtin.buffers, {})
+vim.keymap.set("n", "<C-p>", builtin.find_files, {})
+vim.keymap.set("n", "<C-f>", builtin.live_grep, {})
+vim.keymap.set("n", "<C-b>", builtin.buffers, {})
 
 require("nvim-treesitter.configs").setup({
   ensure_installed = {
