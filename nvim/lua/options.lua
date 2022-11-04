8b86154ac5fd8c9eda88e9a052442a99daffa528
David Tranaeus <david.tranaeus@kundo.se>
2022-11-04 11:44:44 +0100

    Add $RC

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -21,7 +21,8 @@ vim.api.nvim_create_autocmd("FileType", {
 })
 
 vim.cmd("filetype indent off") -- default indent is bonkers for py
-vim.cmd("command Config e ~/.config/nvim/")
+
+vim.env.RC = "$HOME/.config/nvim/"
 
 -- nvims py executable for py plugins, :h python-virtualenv
 vim.g.python3_host_prog = "~/.pyenv/versions/neovim/bin/python"
